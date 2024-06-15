b6f1dc689baca7312dc6bedcc59517f84e6f7443
rarestzhou <rarestzhou@gmail.com>
2024-06-15 22:33:53 +0800

    rollback

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,47 +1,46 @@
-// TODO by tibidabo: sitemap xml file
-// import { type MetadataRoute } from 'next';
-// import { locales } from '@/i18n';
+import { type MetadataRoute } from 'next';
+import { locales } from '@/i18n';
 
-// import { BASE_URL } from '@/lib/env';
+import { BASE_URL } from '@/lib/env';
 
-// export default function sitemap(): MetadataRoute.Sitemap {
-//   const sitemapRoutes: MetadataRoute.Sitemap = [
-//     {
-//       url: '', // home
-//       lastModified: new Date(),
-//       changeFrequency: 'daily',
-//       priority: 1,
-//     },
-//     {
-//       url: 'explore',
-//       lastModified: new Date(),
-//       changeFrequency: 'daily',
-//       priority: 1,
-//     },
-//     {
-//       url: 'submit',
-//       lastModified: new Date(),
-//       changeFrequency: 'monthly',
-//       priority: 0.8,
-//     },
-//     {
-//       url: 'startup',
-//       lastModified: new Date(),
-//       changeFrequency: 'daily',
-//       priority: 0.8,
-//     },
-//   ];
+export default function sitemap(): MetadataRoute.Sitemap {
+  const sitemapRoutes: MetadataRoute.Sitemap = [
+    {
+      url: '', // home
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+    {
+      url: 'explore',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+    {
+      url: 'submit',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'startup',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.8,
+    },
+  ];
 
-//   const sitemapData = sitemapRoutes.flatMap((route) =>
-//     locales.map((locale) => {
-//       const lang = locale === 'en' ? '' : `/${locale}`;
-//       const routeUrl = route.url === '' ? '' : `/${route.url}`;
-//       return {
-//         ...route,
-//         url: `${BASE_URL}${lang}${routeUrl}`,
-//       };
-//     }),
-//   );
+  const sitemapData = sitemapRoutes.flatMap((route) =>
+    locales.map((locale) => {
+      const lang = locale === 'en' ? '' : `/${locale}`;
+      const routeUrl = route.url === '' ? '' : `/${route.url}`;
+      return {
+        ...route,
+        url: `${BASE_URL}${lang}${routeUrl}`,
+      };
+    }),
+  );
 
-//   return sitemapData;
-// }
+  return sitemapData;
+}
